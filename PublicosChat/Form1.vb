40924f56157f489fdb2a171ecb20108047b4a050
João Dubas <JoaoDlks@gmail.com>
2017-08-04 22:47:07 -0300

    Alteracao Scrool2

--- PublicosChat/Form1.vb
+++ PublicosChat/Form1.vb
@@ -5,6 +5,9 @@ Imports System.Text
 Public Class Form1
     Public flagExiteChat As Boolean
     Public caminho, usuario As String
+    Dim Leitor As System.IO.FileInfo
+    Dim infoReader As System.IO.FileInfo
+    Public Teste As Date
 
     Private Sub btnCriarSala_Click(sender As Object, e As EventArgs) Handles btnCriarSala.Click
         Dim nomeNovoChat = InputBox("Digite o nome da nova sala: ", "Nova Sala")
@@ -29,6 +32,14 @@ Public Class Form1
         If (flagExiteChat = True) Then
             Using sr As StreamReader = File.OpenText(caminho)
                 txtChat.Text = sr.ReadToEnd
+                infoReader = My.Computer.FileSystem.GetFileInfo(caminho)
+
+                If (Teste = infoReader.LastWriteTime) Then
+                    txtChat.Text = txtChat.Text & Chr(13) & Chr(10)
+                    txtChat.SelectionStart = 10
+                Else
+                    Teste = infoReader.LastWriteTime
+                End If
             End Using
         End If
     End Sub
@@ -36,6 +47,7 @@ Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         flagExiteChat = False
         usuario = InputBox("Digite seu usuário: ", "Usuário")
+
     End Sub
 
     Private Sub txtMensagem_TextChanged(sender As Object, e As EventArgs) Handles txtMensagem.TextChanged
